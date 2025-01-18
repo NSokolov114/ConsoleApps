13638e2bb2f35e30ed53bdac80a75740504cde6c
NSokolov114 <sokoloff114@gmail.com>
2025-01-17 21:09:54 -0500

    Add project files.

---
+++ ToDoList/Program.cs
@@ -0,0 +1,8 @@
+﻿void PrintOptions()
+{
+    Console.WriteLine("What do you want to do?");
+    Console.WriteLine("[S]ee all TODOs");
+    Console.WriteLine("[A]dd a TODO");
+    Console.WriteLine("[R]emove a TODO");
+    Console.WriteLine("[E]xit");
+}
