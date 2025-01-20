1a645bfa9b289bce301ba8b94ed98ffde52333ed
NSokolov114 <sokoloff114@gmail.com>
2025-01-19 20:34:00 -0500

    move excel-to-xml converter

---
+++ ExcelXMLConverter/Program.cs
@@ -0,0 +1,59 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Linq;
+using System.Runtime.InteropServices;
+//using System.Text;
+//using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+static void readExcelFile()
+{
+
+    // create COM Objects
+    Excel.Application xlApp = new Excel.Application();
+    Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"C:\Users\Nick\source" +
+@"\repos\ExcelXMLConverter\data\sheet.xlsx");
+    Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+    Excel.Range xlRange = xlWorksheet.UsedRange;
+
+    int rowCount = xlRange.Rows.Count;
+    int colCount = xlRange.Columns.Count;
+
+    // print file
+    for (int i = 1; i <= rowCount; i++)
+    {
+        for (int j = 1; j <= colCount; j++)
+        {
+            if (j == 1)
+                Console.Write("\r\n");
+
+            if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
+                Console.Write(xlRange.Cells[i, j].Value2.ToString() + "\t");
+        }
+    }
+
+    // cleanup - ???
+    GC.Collect();
+    GC.WaitForPendingFinalizers();
+
+    // rule of thumb for releasing com objects:
+    // never use two dots, all COM objects must be referenced and released individually
+    // ex: [something].[something].[something] is bad
+
+    // release com objects to fully kill excel process from running in the background
+    Marshal.ReleaseComObject(xlRange);
+    Marshal.ReleaseComObject(xlWorksheet);
+
+    // close and release
+    xlWorkbook.Close();
+    Marshal.ReleaseComObject(xlWorkbook);
+
+    // quit and release
+    xlApp.Quit();
+    Marshal.ReleaseComObject(xlApp);
+}
+
+readExcelFile();
+
+Console.ReadKey();
---
+++ playground/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+//var bd = new DateTime(2025, 4, 4, 12, 34, 56);
+
+//Console.WriteLine(bd.DayOfYear); // 94
+
+//var nextBd = bd.AddYears(1);
+
+//Console.WriteLine(bd);
+
+var rect1 = new Rectangle(5, 10);
+Console.WriteLine($"Area is: {rect1.Width * rect1.Height}");
+
+
+Console.ReadKey();
+
+class Rectangle
+{
+    public int Width;
+    public int Height;
+
+    public Rectangle(int width, int height)
+    {
+        Width = width;
+        Height = height;
+    }
+}
